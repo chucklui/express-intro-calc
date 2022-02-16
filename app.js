27c854c1b42b03588d048235519b3b27f8a06031
brendan <lobrendan@gmail.com>
2022-02-16 12:40:36 -0800

    working on further study

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /** Simple demo Express app. */
 const { findMean, findMode, findMedian } = require("./stats");
 const express = require("express");
-const {convertStrNums} = require("./utils");
+const { convertStrNums, writeToFile } = require("./utils");
 const app = express();
 
 app.use(express.json());
@@ -12,7 +12,7 @@ const { NotFoundError, BadRequestError } = require("./expressError");
 const MISSING = "Expected key `nums` with comma-separated list of numbers.";
 
 /** Finds mean of nums in querystring: returns {operation: "mean", result } */
-app.get("/mean", function (req, res) {
+app.get("/mean", async function (req, res) {
   // pull and convert incoming numbers into an array of numbers
   const data = req.query.nums;
   if (!data) throw new BadRequestError(MISSING);
@@ -21,8 +21,14 @@ app.get("/mean", function (req, res) {
   const dataArr = data.split(',');
   const dataArrNums = convertStrNums(dataArr);
   const mean = findMean(dataArrNums);
+  const json = { response: { operation: "mean", value: mean } };
 
-  return res.json({ response: { operation: "mean", value: mean } });
+  // console.log(req.query.save);
+  // if (req.query.save) {
+  //   writeToFile(dataArrNums);
+  // }
+  return res.json(json);
+
 })
 
 
@@ -38,12 +44,48 @@ app.get("/median", function (req, res) {
   // convert data into an array of objects
   const dataArr = data.split(',');
   const dataArrNums = convertStrNums(dataArr);
-  const mean = findMedian(dataArrNums);
+  const median = findMedian(dataArrNums);
 
-  return res.json({ response: { operation: "median", value: mean } });
+  return res.json({ response: { operation: "median", value: median } });
 })
 
-/** Finds mode of nums in qs: returns {operation: "mean", result } */
+/** Finds mode of nums in qs: returns {operation: "mode", result } */
+app.get("/mode", function (req, res) {
+  // pull and convert incoming numbers into an array of numbers
+  const data = req.query.nums;
+
+  if (!data) {
+    throw new BadRequestError(MISSING);
+  }
+
+  // convert data into an array of objects
+  const dataArr = data.split(',');
+  const dataArrNums = convertStrNums(dataArr);
+  const mode = findMode(dataArrNums);
+
+  return res.json({ response: { operation: "mode", value: mode } });
+})
+
+/** Finds mean, median, and mode of nums in qs: returns 
+ * {operation: "all", mean:mean, median:median, mode:mode } */
+app.get("/all", function (req, res) {
+  // pull and convert incoming numbers into an array of numbers
+  const data = req.query.nums;
+
+  if (!data) {
+    throw new BadRequestError(MISSING);
+  }
+
+  // convert data into an array of objects
+  const dataArr = data.split(',');
+  const dataArrNums = convertStrNums(dataArr);
+  const mode = findMode(dataArrNums);
+  const median = findMedian(dataArrNums);
+  const mean = findMean(dataArrNums);
+
+  return res.json({ response: { operation: "all", mean, median, mode } });
+})
+
 
 
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 const { BadRequestError } = require("./expressError");
-
+const fsP = require("fs/promises");
 
 /** Convert strNums like ["1","2","3"] to [1, 2, 3]. */
 
@@ -7,14 +7,25 @@ function convertStrNums(strNums) {
   // if the conversion isn't successful, throw a BadRequestError and will
   // be handled in your route
   const dataArrNums = strNums.map(function (str) {
-    if(isNaN(parseInt(str))){
+    if (isNaN(parseInt(str))) {
       throw new BadRequestError(`${str} is not a number.`);
     }
     return parseInt(str);
   });
-  
+
   return dataArrNums;
 }
 
 
-module.exports = { convertStrNums };
+/** Takes an input of json and saves to file */
+async function writeToFile(json) {
+  try {
+    await fsP.writeFile("./results.json", json, "utf8");
+  }
+  catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+module.exports = { convertStrNums, writeToFile };
